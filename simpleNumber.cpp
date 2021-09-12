dde6d270f33b2284be3b3cdd7551013efa0fab2f
Ershov Daniil <danil.ershov2001@mail.ru>
2021-09-12 18:02:26 +0300

    first commit

---
+++ simpleNumber.cpp
@@ -0,0 +1,54 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <cctype>
+#include <cstdlib> 
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i=0;
+	bool fDigit=0;
+	bool fSimple=0;
+	int n=0;
+	int k=1;
+	std::string str="";
+	std::cout << "vvedi chislo\n";
+	std::cin >> str;
+	while (i < str.length() && fDigit==0)
+	{
+		if (!isdigit(str[i]))
+        {
+			fDigit=1;
+		}
+		i++;
+	}
+	if (fDigit)
+	{
+		std::cout << "neverno vvedeno chislo\n";
+	} else {
+		n = stoi(str);
+		while (k*k <= n && fSimple != 1)
+		{
+			if (n % k == 0 && k != n && k != 1)
+			{
+				fSimple = 1;
+			}
+			k++;
+		}
+	}
+	if (fSimple)
+	{
+		std::cout << "ne prostoe";
+	} else {
+		std:: cout << "prostoe";
+	}
+	return 0;
+}
+
+"# SimpleNumbers" 
+"# SimpleNumbers" 
